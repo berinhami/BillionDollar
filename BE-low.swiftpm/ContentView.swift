8920486dc03723dc092bc3e3eee716e0c1424e11
Selena Montoya <selenamontoya85@yahoo.com>
2022-05-11 15:20:06 -0600

    swift setup
    configured file structure and storyboards

---
+++ BE-low.swiftpm/ContentView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+//
+//var bac: Float
+//
+//switch bac{
+//case 0.0 ... 0.03:
+//    print("yikes you are drunk")
+//}
+
+func blow(){
+    print("this worked")
+}
+
+blow()
---
+++ Below/Below/testPlay.swift
@@ -0,0 +1,16 @@
+//
+//  testPlay.swift
+//  Below
+//
+//  Created by Selena Montoya on 5/11/22.
+//
+
+let bunchOfWords = ["hello", "hi", "bruh"]
+
+func forLoop(){
+    
+    for word in bunchOfWords{
+        print(word)
+    }
+
+}
