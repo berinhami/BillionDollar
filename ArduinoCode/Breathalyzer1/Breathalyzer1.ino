9411e56dd000418cf8550643181ae23ad17eee9d
Treg Townsend <81056599+tregt15@users.noreply.github.com>
2022-05-14 15:22:45 -0600

    Arduino Testing
    Testing Arduino Code

--- ArduinoCode/Breathalyzer1/Breathalyzer1.ino
+++ ArduinoCode/Breathalyzer1/Breathalyzer1.ino
@@ -11,6 +11,7 @@ void setup() {
 Serial.begin(9600);
 pinMode(DOUTpin, INPUT);//sets the pin as an input to the arduino
 pinMode(ledPin, OUTPUT);//sets the pin as an output of the arduino
+Serial.println("test");
 }
 
 void loop()
@@ -24,22 +25,7 @@ if(bacvalue<0.03)
       printbacvalue = 0;
       }
 }
-if (value>=200 && value<280)
-{
-    Serial.println("Alcohol detected");
-}
-if (value>=280 && value<350)
-{
-    Serial.println("More than one drink going on here....");
-}
-if (value>=350 && value <450)
-{
-    Serial.println("Serious Booze here! ");
-}
-if(value>450)
-{
-   Serial.println("You are drunk!");
-}
+
 
 delay (500);
 }
