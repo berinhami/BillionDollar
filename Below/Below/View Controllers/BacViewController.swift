774007e5ed7e70e846d3cc539f319ef61841bb36
Selena Montoya <selenamontoya85@yahoo.com>
2022-06-02 13:35:24 -0600

    storyboard
    set up 2 storyboards

---
+++ Below/Below/View Controllers/BacViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BacViewController.swift
+//  Below
+//
+//  Created by Selena Montoya on 5/12/22.
+//
+
+import UIKit
+
+class BacViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Below/Below/View Controllers/ViewController.swift
+++ Below/Below/View Controllers/ViewController.swift
@@ -11,9 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+    
     }
-
+    @IBAction func navButtons(){
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "high_bac") as! BacViewController
+        present( vc, animated: true)
+    }
 
 }
 
